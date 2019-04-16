65e5a3622e500e6692ef9bc203ee968272f5c9e6
Bud Aminof <budaminof@gmail.com>
2019-04-16 14:07:47 -0600

    musicInput: validate the user input adds onChange with validation of the user input disable submit button if there are no notes adds test for onChange logic and button disable logic
    NOTE: for now if the character is not valid we do not print it, should we change this behavior?

--- src/musicInput/musicInput.component.js
+++ src/musicInput/musicInput.component.js
@@ -2,13 +2,36 @@ import React from 'react';
 import './musicInput.scss';
 
 class MusicInput extends React.Component {
+  state = {
+    notes: 'AEC',
+    placeholder: 'AEC',
+    allowedNoted: ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
+    showError: false,
+  }
+
+  handleChange = (event) => {
+    const charArray = event.target.value.toUpperCase().split('')
+    const validChars = charArray.every((char) => this.state.allowedNoted.includes(char) ? true : false)
+
+    if (!validChars) {
+      this.setState({ showError: true })
+    } else {
+      this.setState({ notes: charArray.join(''), showError: false })
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
 
   render() {
     return (
-      <div className="music-input">
-        <input type="text" size="40" />
-        <button>play music</button>
-      </div>
+      <form className="music-input" onSubmit={this.handleSubmit}>
+        <input type="text" size="40" value={this.state.notes} onChange={this.handleChange} />
+        {this.state.showError ? <p>You can only use the notes displayed on the piano.</p> : <p></p>}
+        <button type="submit" disabled={!this.state.notes.length}>play</button>
+      </form>
     )
   }
 }
--- src/musicInput/musicInput.spec.js
+++ src/musicInput/musicInput.spec.js
@@ -3,12 +3,45 @@ import { shallow } from 'enzyme';
 import MusicInput from './musicInput.component';
 
 describe('MUSIC INPUT', () => {
-  const wrapper = shallow(<MusicInput />);
-  it('should exists', () => {
-    console.log(wrapper.children().length);
 
+  it('onChange should accept only valid notes from the octave', () => {
+    const wrapper = shallow(<MusicInput />);
+    const instance = wrapper.instance();
+
+    instance.handleChange({ target: { value: 'A' } });
+    expect(instance.state.showError).toBeFalsy();
+    expect(instance.state.notes).toBe('A');
+
+    instance.handleChange({ target: { value: 'Ab' } });
+    expect(instance.state.showError).toBeFalsy();
+    expect(instance.state.notes).toBe('AB');
+
+    instance.handleChange({ target: { value: 'Ab7' } });
+    expect(instance.state.showError).toBeTruthy();
+    expect(instance.state.notes).toBe('AB');
   })
-  it('should save the user input on click', () => {
 
-  });
+  it('onChange should transform all notes to uppercase', () => {
+    const wrapper = shallow(<MusicInput />);
+    const instance = wrapper.instance();
+
+    instance.handleChange({ target: { value: 'e' } });
+    expect(instance.state.notes).toBe('E');
+
+    instance.handleChange({ target: { value: 'ab' } });
+    expect(instance.state.notes).toBe('AB');
+
+    instance.handleChange({ target: { value: 'ab12' } });
+    expect(instance.state.notes).toBe('AB');
+  })
+
+  it('should disable the PLAY button if there are no notes', () => {
+    const wrapper = shallow(<MusicInput />);
+    const instance = wrapper.instance();
+
+    instance.handleChange({ target: { value: 'ABC' } });
+    expect(wrapper.find('button').is('[disabled]')).toBe(false);
+    instance.handleChange({ target: { value: '' } });
+    expect(wrapper.find('button').is('[disabled]')).toBe(true);
+  })
 });
